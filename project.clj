44f2056d62f44708e75cec891d9c976020118a00
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-04-27 08:48:21 +0200

    [release] 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chart-utils "1.0.0-SNAPSHOT"
+(defproject chart-utils "1.0.0"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.jfree/jfreechart "1.0.14"]])
