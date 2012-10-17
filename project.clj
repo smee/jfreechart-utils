7d2ed83151f41c5c97a3bc6883144e069930e421
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-10-17 17:20:56 +0200

    [internal] remove superfluous pdf dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject chart-utils "1.0.2-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.jfree/jfreechart "1.0.14"]])
+                 [org.jfree/jfreechart "1.0.14"
+                  :exclusions [com.lowagie/itext
+                               xml-apis]]])
