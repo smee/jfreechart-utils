2a617873aaaf2bcfdaa1360bc12e664dc6741518
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2013-09-18 10:38:42 +0200

    [build] update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chart-utils "1.0.2-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.jfree/jfreechart "1.0.14"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.jfree/jfreechart "1.0.15"
                   :exclusions [com.lowagie/itext
                                xml-apis]]])
