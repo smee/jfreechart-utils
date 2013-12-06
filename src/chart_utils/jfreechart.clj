d62377d6ae48a7172490b6ef92235b12639f5f2d
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2013-12-06 15:16:59 +0100

    [feature] add `bounded-bin-fn` that restricts all values that would be out of bounds by returning the nearest valid bin

--- src/chart_utils/jfreechart.clj
+++ src/chart_utils/jfreechart.clj
@@ -388,9 +388,28 @@ Example:
     ((bin-fn 0 10 2) 5.5) 
     => [4 6]"
   [min max step] 
-  (let [steps (partition 2 1 (range min max step))] 
-    (fn [v] (let [bin-start (int (/ (- v min) step))] 
-              [(+ min (* bin-start step)) (+ min (* (inc bin-start) step))]))))
+  (fn [v] (let [bin-start (int (/ (- v min) step))] 
+            [(+ min (* bin-start step)) (+ min (* (inc bin-start) step))])))
+
+(defn bounded-bin-fn 
+  "Create a function that returns the interval that a value x falls in. If the value is out of bounds,
+returns the nearest valid bin.
+Example:  
+    ((bounded-bin-fn 0 10 2) 5.5) 
+    => [4 6]
+    ((bounded-bin-fn 0 10 2) -5) 
+    => [0 2]
+    ((bounded-bin-fn 0 10 2) 100) 
+    => [8 10]
+"
+  [minimum maximum step] 
+  {:pre [(every? number? [minimum maximum step]) 
+         (< minimum maximum)
+         (<= step (- maximum minimum))]} 
+  (let [supremum-bin (dec (int (/ (- maximum minimum) step)))] 
+    (fn [v] (let [bin-start (int (/ (- v minimum) step))
+                  bin-start (-> bin-start (max 0) (min supremum-bin))] 
+              [(+ minimum (* bin-start step)) (+ minimum (* (inc bin-start) step))]))))
 
 (defn- map-values 
   "Change all values or all keys and values by applying a function to each of them."
