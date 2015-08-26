af20b27381302f2bb1e4ebdc9a516423f711493d
Steffen Dienst <steffen.dienst@uni-leipzig.de>
2015-08-26 14:25:04 +0200

    [feature] axis labels for 3d plots

--- src/chart_utils/3dplot.clj
+++ src/chart_utils/3dplot.clj
@@ -7,7 +7,11 @@
            org.jzy3d.chart.ChartLauncher))
 
 
-(defn create-3d-scatter-plot [points colors & {:keys [size] :or {size 1.0}}]
+(defn create-3d-scatter-plot [points colors {:keys [size x-label y-label z-label]
+                                             :or {size 1.0
+                                                  x-label "X"
+                                                  y-label "Y"
+                                                  z-label "Z"}}]
   (let [n (count points)
         coords (make-array Coord3d n)
         cols (make-array Color n)]
@@ -19,15 +23,23 @@
           (aset cols idx (Color. r g b a))
           (recur (next points) (next colors) (inc idx)))
         (doto (AWTChartComponentFactory/chart Quality/Advanced "newt")
+          (.. getAxeLayout (setXAxeLabel x-label))
+          (.. getAxeLayout (setYAxeLabel y-label))
+          (.. getAxeLayout (setZAxeLabel z-label))
           (.. getScene (add (doto (Scatter. coords cols)
                               (.setWidth (float size))))))))))
+
 
 (defn view [chart]
   (let [rect (org.jzy3d.maths.Rectangle. 200 200 800 600)]
     (ChartLauncher/instructions)
     (ChartLauncher/openChart chart rect "Scatter")))
 (comment
-  
-  (view (create-3d-scatter-plot [[0.0 0.0 0.0] [1.0 1.0 1.0] [5.0 5.0 5.0]]
-                          [[255 0 0 255] [0 255 0 255] [0 0 255 255]]))
+  (view (create-3d-scatter-plot [[0.0 0.0 0.0]
+                                 [1.0 1.0 1.0]
+                                 [5.0 5.0 5.0]]
+                                [[255 0 0 255]
+                                 [0 255 0 255]
+                                 [0 0 255 255]]
+                                :size 8.0))
   )
